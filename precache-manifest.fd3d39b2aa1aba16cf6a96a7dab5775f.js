affe5b557b048104cf964123cccd83cb4583de5a
ndiyo <ndiyoedward@gmail.com>
2024-08-04 18:20:57 +0100

    Updates

---
+++ precache-manifest.fd3d39b2aa1aba16cf6a96a7dab5775f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfd17ad1cb1114998fded2b67e15d37",
+    "url": "/DICED/index.html"
+  },
+  {
+    "revision": "84e9f27791fe17936663",
+    "url": "/DICED/static/css/main.c30551eb.chunk.css"
+  },
+  {
+    "revision": "dd34dd379284b2f84412",
+    "url": "/DICED/static/js/2.23f64e0e.chunk.js"
+  },
+  {
+    "revision": "311fdf2176b69a5a30ee",
+    "url": "/DICED/static/js/3.2ee60a5f.chunk.js"
+  },
+  {
+    "revision": "84e9f27791fe17936663",
+    "url": "/DICED/static/js/main.1466c361.chunk.js"
+  },
+  {
+    "revision": "83b497294c53ec3c45fa",
+    "url": "/DICED/static/js/runtime~main.2a007eee.js"
+  },
+  {
+    "revision": "e9ccc8830475d128ddab682d5253bb29",
+    "url": "/DICED/static/media/717fd4d6f85a77a6eaebc9eec0dfcd69.e9ccc883.jpg"
+  },
+  {
+    "revision": "6430c9f36d84c2df65dddbf465f3fa76",
+    "url": "/DICED/static/media/779357845d07c9a55401879b03c732d5.6430c9f3.jpg"
+  },
+  {
+    "revision": "69e0a8941f621b31093b04d90334f3db",
+    "url": "/DICED/static/media/98eb09406e4537d1087bfea99bd5db79.69e0a894.jpg"
+  },
+  {
+    "revision": "2ccb2cffcad52283bf96acb15bb33ade",
+    "url": "/DICED/static/media/Rolling_Dice_by_ianllanason_DeviantArt.2ccb2cff.jpg"
+  },
+  {
+    "revision": "6363a05150eaa68be72e14d49a728fc3",
+    "url": "/DICED/static/media/Rpg_Dice_Drawing.6363a051.jpg"
+  }
+]);
