71e8d86774888ae60f6ad1713435336cce29bb34
Edward Ndiyo <52774709+Edwardndiyo@users.noreply.github.com>
2024-08-04 20:58:49 +0100

    Update ContactUsPage.js

--- src/components/ContactUsPage.js
+++ src/components/ContactUsPage.js
@@ -29,7 +29,7 @@ const ContactUsPage = () => {
     // window.open(whatsappUrl, '_blank');
 
     try {
-      const response = await axios.post(' http://edwardndiyoo.pythonanywhere.com/contact', formData);
+      const response = await axios.post(' https://edwardndiyoo.pythonanywhere.com/contact', formData);
       alert(response.data.message);
     } catch (error) {
       console.error('Error:', error);
