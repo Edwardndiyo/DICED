17aa1f0d3f51d0a7564ec6430e9e5417f653f854
Edward Ndiyo <52774709+Edwardndiyo@users.noreply.github.com>
2024-08-04 20:57:41 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://edwardndiyoo.pythonanywhere.com',
+  baseURL: 'https://edwardndiyoo.pythonanywhere.com',
 });
 
 export default api;
